5d7bc6a13bd8f3b73f1cac3cd5f9cb70638ec817
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-06-01 12:49:54 -0300

    fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,11 @@ gulp.task('deploy-gh-pages', function () {
   require('fs').writeFileSync('./build/CNAME', 'simple-dai-portal.makerdao.com');
   return gulp.src('./build/**/*')
     .pipe(ghPages())
-})
+});
 
-gulp.task('deploy-surge', [], function () {
+gulp.task('deploy-surge', function () {
   return surge({
     project: './build',                           // Path to your static build directory
     domain: 'https://simple-dai-portal.surge.sh'  // Your domain or Surge subdomain
   })
-})
+});
