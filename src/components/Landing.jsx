7cb3c4800ab907f122f7bad7e33450dad924453e
Michael Elliot <michael@digital.com.au>
2018-06-08 12:53:16 +0800

    Refactor landing page and styles.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -28,61 +28,63 @@ class Landing extends React.Component {
     };
     return (
       <div className="landing">
-        <h1>Welcome to the<br />Collateralized Debt Position Portal</h1>
-        <Slider {...settings} className="landing-slider">
-          <div className="first-slide">
-            <div style={ {textAlign: 'center'} }>
-              <img className="preview" src="../img/landing-preview.png" alt="CDP Portal" />
-              <p className="align-center">Is Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+        <div className="landing-body">
+          <h1>Welcome to the<br />Collateralized Debt Position Portal</h1>
+          <Slider {...settings} className="landing-slider">
+            <div className="first-slide">
+              <div style={ {textAlign: 'center'} }>
+                <img className="preview" src="../img/landing-preview.png" alt="CDP Portal" />
+                <p className="align-center">Is Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="info-slide">
-              <h1>01.<span className="line"></span>THE CONCEPT</h1>
-              <h2 className="sm">What is a Collateralized Debt Position (CDP)?</h2>
-              <p>A CDP enables the generation of DAI stablecoins against the collateral (currently ETH) that you lock up in the CDP until you pay back the DAI you generated. Is Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+            <div>
+              <div className="info-slide">
+                <h1>01.<span className="line"></span>THE CONCEPT</h1>
+                <h2 className="sm">What is a Collateralized Debt Position (CDP)?</h2>
+                <p>A CDP enables the generation of DAI stablecoins against the collateral (currently ETH) that you lock up in the CDP until you pay back the DAI you generated. Is Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="info-slide">
-              <h1>02.<span className="line"></span>THE BENEFITS</h1>
-              <h2>What are the benefits of opening a CDP?</h2>
-              <p>You can get liquidity by generating DAI stablecoins without giving up ownership of your collateral (as long as you make sure that the CDP holds enough collateral to cover the value of the DAI stablecoins). When using ETH as collateral, the value of the locked up collateral must always be more than 150% of the amount of DAI stablecoins that you generate.</p>
+            <div>
+              <div className="info-slide">
+                <h1>02.<span className="line"></span>THE BENEFITS</h1>
+                <h2>What are the benefits of opening a CDP?</h2>
+                <p>You can get liquidity by generating DAI stablecoins without giving up ownership of your collateral (as long as you make sure that the CDP holds enough collateral to cover the value of the DAI stablecoins). When using ETH as collateral, the value of the locked up collateral must always be more than 150% of the amount of DAI stablecoins that you generate.</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="info-slide">
-              <h1>03.<span className="line"></span>THE SETUP</h1>
-              <h2>How does it work?</h2>
-              <p>You determine how much ETH you want to lock up in the CDP. You generate DAI stablecoins against the ETH you locked up, and spend them as you wish. You pay back the DAI stablecoins when you no longer need the liquidity, together with a small stability fee, and then you can withdraw the collateral that you locked up.</p>
+            <div>
+              <div className="info-slide">
+                <h1>03.<span className="line"></span>THE SETUP</h1>
+                <h2>How does it work?</h2>
+                <p>You determine how much ETH you want to lock up in the CDP. You generate DAI stablecoins against the ETH you locked up, and spend them as you wish. You pay back the DAI stablecoins when you no longer need the liquidity, together with a small stability fee, and then you can withdraw the collateral that you locked up.</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="info-slide">
-              <h1>04.<span className="line"></span>THE RISKS</h1>
-              <h2>Is there any risk involved in creating a CDP?</h2>
-              <p>As long as you monitor your CDP and make sure that the value of the locked up ETH is always more than 150% of the DAI stablecoins that you have generated, there is no risk. If the value of the collateral comes close to 150% you can add more collateral, or pay back some of the debt. If the value of the locked up collateral falls below 150% then your CDP will be liquidated. This means that your collateral is being sold by the system in order to cover the value of the Dai stablecoins that you generated. Any leftover collateral is returned to your CDP so you can withdraw it.</p>
+            <div>
+              <div className="info-slide">
+                <h1>04.<span className="line"></span>THE RISKS</h1>
+                <h2>Is there any risk involved in creating a CDP?</h2>
+                <p>As long as you monitor your CDP and make sure that the value of the locked up ETH is always more than 150% of the DAI stablecoins that you have generated, there is no risk. If the value of the collateral comes close to 150% you can add more collateral, or pay back some of the debt. If the value of the locked up collateral falls below 150% then your CDP will be liquidated. This means that your collateral is being sold by the system in order to cover the value of the Dai stablecoins that you generated. Any leftover collateral is returned to your CDP so you can withdraw it.</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="info-slide">
-              <h1>05.<span className="line"></span>THE COST</h1>
-              <h2>Does it cost anything?</h2>
-              <p>There is a stability fee on the Dai stablecoins of 0.05% per year. You pay the fee with MKR when you pay back the Dai. If your CDP becomes liquidated, then there is a 13% liquidation penalty that will be subtracted when the locked collateral is sold. Is Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+            <div>
+              <div className="info-slide">
+                <h1>05.<span className="line"></span>THE COST</h1>
+                <h2>Does it cost anything?</h2>
+                <p>There is a stability fee on the Dai stablecoins of 0.05% per year. You pay the fee with MKR when you pay back the Dai. If your CDP becomes liquidated, then there is a 13% liquidation penalty that will be subtracted when the locked collateral is sold. Is Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+              </div>
             </div>
-          </div>
-        </Slider>
+          </Slider>
 
-        <div className="getting-started">
-          <h1>What do I need to get started?</h1>
-          <p>Connect one of the four wallets below to automatically connect to the CDP Portal. For more information on these wallets, use the links below. If you are an advanced user you can also use the MakerDAO command line interface (CLI) to create CDPs.</p>
-          <p className="align-center"><a className="faq">See FAQs for additional information on wallets</a></p>
-          <ul>
-            <li><a href="https://metamask.io/"><img src="../img/metamask-logo.svg" alt="Get MetaMask" /><div>Get MetaMask</div></a></li>
-            <li><a href="https://www.parity.io/"><img src="../img/parity-logo.png" alt="Get Parity" /><div>Get Parity</div></a></li>
-            <li><a href="https://www.ledgerwallet.com/products/ledger-nano-s"><img src="../img/ledger-nano-logo.png" alt="Get Ledger Nano S" /><div>Get Ledger Nano S</div></a></li>
-            <li><a href="https://trezor.io/"><img src="../img/trezor-logo.png" alt="Get Trezor" /><div>Get Trezor</div></a></li>
-          </ul>
+          <div className="getting-started">
+            <h1>What do I need to get started?</h1>
+            <p>Connect one of the four wallets below to automatically connect to the CDP Portal. For more information on these wallets, use the links below. If you are an advanced user you can also use the MakerDAO command line interface (CLI) to create CDPs.</p>
+            <p className="align-center"><a className="faq">See FAQs for additional information on wallets</a></p>
+            <ul>
+              <li><a href="https://metamask.io/"><img src="../img/metamask-logo.svg" alt="Get MetaMask" /><div>Get MetaMask</div></a></li>
+              <li><a href="https://www.parity.io/"><img src="../img/parity-logo.png" alt="Get Parity" /><div>Get Parity</div></a></li>
+              <li><a href="https://www.ledgerwallet.com/products/ledger-nano-s"><img src="../img/ledger-nano-logo.png" alt="Get Ledger Nano S" /><div>Get Ledger Nano S</div></a></li>
+              <li><a href="https://trezor.io/"><img src="../img/trezor-logo.png" alt="Get Trezor" /><div>Get Trezor</div></a></li>
+            </ul>
+          </div>
         </div>
 
         <div className="landing-footer">
