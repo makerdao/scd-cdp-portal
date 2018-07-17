5228345b8c257e4d0a27f6a0d2a66a8d6bbe2f95
Michael Elliot <michael@digital.com.au>
2018-07-17 10:43:45 +0800

    Add class prop to InlineNotification component

--- src/components/InlineNotification.jsx
+++ src/components/InlineNotification.jsx
@@ -9,7 +9,7 @@ const icon = (
 class InlineNotification extends React.Component {
   render() {
     return (
-      <div className={ "inline-notification " + (this.props.type === "warning" ? "is-warning" : "") }>
+      <div className={ "inline-notification " + (this.props.type === "warning" ? "is-warning" : "") + (this.props.class ? " " + this.props.class : "") }>
         {
           this.props.onCloseButtonClick &&
           <svg onClick={ e => { e.preventDefault(); this.props.onCloseButtonClick() } } className="close-button" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
