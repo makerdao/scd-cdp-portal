190fb2797001e12b5c96e8f8050b698ec48d56bf
Michael Elliot <michael@digital.com.au>
2018-07-17 13:33:54 +0800

    DD-24: Style migrate CDPs page

--- src/components/LegacyCups.jsx
+++ src/components/LegacyCups.jsx
@@ -7,15 +7,15 @@ class LegacyCups extends React.Component {
   render() {
     return (
       Object.keys(this.props.system.tub.legacyCups).length > 0 &&
-      <div>
+      <div className="migrate-cups-section">
         <header className="col">
           <h1 className="typo-h1 inline-headline">Migrate CDPs</h1>
         </header>
-        Your account has { this.props.system.tub.legacyCups.length } existing CDPs to be migrated.
+        <div className="number-of-cdps-to-migrate">Your account has <b>{ Object.keys(this.props.system.tub.legacyCups).length }</b> existing { 'CDP' + (Object.keys(this.props.system.tub.legacyCups).length > 1 ? 's' : '') } to be migrated.</div>
         {
           Object.keys(this.props.system.tub.legacyCups).map(key =>
-            <React.Fragment key={ key }>
-              <div>
+            <div className="cup-to-migrate" key={ key }>
+              <div className="cdp-id-heading">
                 CDP ID #{ key }
               </div>
               <table>
@@ -25,7 +25,7 @@ class LegacyCups extends React.Component {
                     <th>Locked PETH</th>
                     <th>% Ratio</th>
                     <th>Liquidation Price</th>
-                    <th>Status</th>
+                    <th className="status-column">Status</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -46,7 +46,7 @@ class LegacyCups extends React.Component {
                     <td>
                       { this.props.system.tub.off === false && this.props.system.tub.legacyCups[key].liq_price && this.props.system.tub.legacyCups[key].liq_price.gt(0) ? printNumber(this.props.system.tub.legacyCups[key].liq_price) : "-" }
                     </td>
-                    <td>
+                    <td className="cdp-status">
                       {
                         this.props.system.tub.off === false
                         ?
@@ -62,11 +62,26 @@ class LegacyCups extends React.Component {
                               ?
                                 this.props.system.tub.legacyCups[key].art.eq(0) || this.props.system.tub.legacyCups[key].ratio.gte(2)
                                 ?
-                                  "Safe"
+                                <React.Fragment>
+                                  <svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="5" cy="5" fill="#1ABC9C" fillRule="evenodd" r="5"/>
+                                  </svg>
+                                  Safe
+                                </React.Fragment>
                                 :
-                                  "Risk"
+                                  <React.Fragment>
+                                    <svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+                                      <circle cx="5" cy="5" fill="#FBAE17" fillRule="evenodd" r="5"/>
+                                    </svg>
+                                    Risk
+                                  </React.Fragment>
                               :
-                                "Unsafe"
+                                <React.Fragment>
+                                  <svg width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="5" cy="5" fill="#C0392B" fillRule="evenodd" r="5"/>
+                                  </svg>
+                                  Unsafe
+                                </React.Fragment>
                         :
                           "-"
                       }
@@ -74,22 +89,21 @@ class LegacyCups extends React.Component {
                   </tr>
                 </tbody>
               </table>
-              <div>
+              <div className="migrate-button">
                 {
                   this.props.system.tub.cups[key]
                   ?
                     "CDP Migrated"
                   :
-                    <button style={ {display: "block"} } data-method="migrate" data-cup={ key } disabled={ this.props.transactions.loading.migrate && this.props.transactions.loading.migrate[key] } onClick={ this.props.dialog.handleOpenDialog }>Migrate CDP #{ key }</button>
+                    <button className="text-btn" data-method="migrate" data-cup={ key } disabled={ this.props.transactions.loading.migrate && this.props.transactions.loading.migrate[key] } onClick={ this.props.dialog.handleOpenDialog }>MIGRATE CDP #{ key }</button>
                 }
               </div>
-            </React.Fragment>
+              <div className="clearfix"></div>
+            </div>
           )
         }
-        <hr />
-        <button onClick={ () => this.props.setOpenMigrate(false) }>
-          Return to Dashboard
-        </button>
+        <div className="clearfix"></div>
+        <button className="bright-style text-btn" style={ {display: 'block', margin: '4rem auto 0'} } onClick={ () => this.props.setOpenMigrate(false) }>RETURN TO DASHBOARD</button>
       </div>
     )
   }
