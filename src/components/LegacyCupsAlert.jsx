4d6362ae9f217da5352ea455c79f4d7154e1b2c1
Michael Elliot <michael@digital.com.au>
2018-07-17 11:02:12 +0800

    DD-57: Style migrate CDPs menu item

--- src/components/LegacyCupsAlert.jsx
+++ src/components/LegacyCupsAlert.jsx
@@ -13,8 +13,9 @@ class LegacyCupsAlert extends React.Component {
     return (
       Object.keys(this.props.system.tub.legacyCups).length > 0 && this.state.show && !localStorage.getItem("LegacyCDPsAlertClosed") &&
       <InlineNotification
+        class="migrate-cups"
         caption="Migrate Existing CDPs"
-          message="Your account has one or more existing CDPs from the old Dai Dashboard. You'll need to migrate these CDPs to the new dashboard to see and interact with them here. Once migrated, your CDPs will only be accesible via this dashboard."
+        message="Your account has one or more existing CDPs from the old Dai Dashboard. You'll need to migrate these CDPs to the new dashboard to see and interact with them here. Once migrated, your CDPs will only be accesible via this dashboard."
         buttonText="Continue"
         onCloseButtonClick={ () => { localStorage.setItem("LegacyCDPsAlertClosed", true); this.setState({show: false}); } }
         onButtonClick={ () => this.props.setOpenMigrate(true) }
