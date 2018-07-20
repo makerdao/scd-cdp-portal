fe83cb887ea44dbc53345e195a4dc482ee152dc4
Michael Elliot <michael@digital.com.au>
2018-07-20 13:28:17 +0800

    Make site more mobile friendly/responsive

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -92,21 +92,6 @@ class Landing extends React.Component {
           </div>
         </div>
 
-        <div className="landing-footer">
-          <div className="logo-block">
-            <div className="line" />
-            <div className="logo-center"><img src="../img/maker-logo-footer.svg" alt="Maker" />&copy; DAI Foundation 2018</div>
-          </div>
-          <p>
-            The DAI Stablecoin System was developed by <a href="https://www.makerdao.com">Maker</a>.<br />
-            Our team consists of developers, economists and designers from all over the world. Our decentralized autonomous organization is governed by our token holders.
-          </p>
-          <ul>
-            <li><a href="">FAQ</a></li>
-            <li><a href="https://www.reddit.com/r/MakerDAO/">Reddit</a></li>
-            <li><a href="https://chat.makerdao.com">Chat</a></li>
-          </ul>
-        </div>
       </div>
     );
   }
