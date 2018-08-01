2a1739f4a35b84605ee22ececee16ba3cf852cf5
Michael Elliot <michael@digital.com.au>
2018-08-01 11:39:10 +0200

    Replace videos section on help page with most asked questions

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -33,19 +33,12 @@ class Help extends React.Component {
                       <li><Link to="/help/what-is-peth">What is PETH?</Link></li>
                       <li><Link to="/help/how-do-i-get-mkr">How do I get MKR tokens?</Link></li>
                     </ul>
+                  </div>
+                  <div className="col col-2 col-extra-padding" style={ {paddingLeft: '2.5em'} }>
+                    <h2 className="typo-h2">&nbsp;</h2>
                     <h3 className="typo-h3 typo-white">Most asked questions</h3>
                     <ul className="typo-cl bullets">
                       <li><Link to="/help/how-does-the-avatar-work">How does the avatar work?</Link></li>
-                    </ul>
-                  </div>
-                  <div className="col col-2 col-extra-padding" style={ {paddingLeft: '2.5em'} }>
-                    <h2 className="typo-h2 ">Videos</h2>
-                    <h3 className="typo-h3 typo-white">First steps</h3>
-                    <ul className="typo-cl bullets">
-                      <li><a href="">What is a Colleteral Dept Position (CDP)?</a></li>
-                      <li><a href="">How do I create a CDP?</a></li>
-                      <li><a href="">How do I generate DAI?</a></li>
-                      <li><a href="">How do I free ETH?</a></li>
                     </ul>
                   </div>
                 </div>
