a3e4a39b32e711a379bb6e33af273bd400a1cd72
Michael Elliot <michael@digital.com.au>
2018-08-16 03:06:25 +0800

    DD-87: Hide Connect Ledger Nano S from dropdown menu in Firefox

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -55,7 +55,7 @@ class Wallet extends React.Component {
     if (getWebClientProviderName() && (getCurrentProviderName() === "ledger" || getCurrentProviderName() === "trezor")) {
       options.push("web");
     }
-    if (getCurrentProviderName() !== "ledger") {
+    if (getCurrentProviderName() !== "ledger" && navigator.userAgent.toLowerCase().indexOf('firefox') === -1) {
       options.push("ledger");
     }
     if (getCurrentProviderName() !== "trezor") {
