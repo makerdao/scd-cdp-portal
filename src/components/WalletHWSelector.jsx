ce826a7c40b1b1333250884b5554b9d582e258f9
Michael Elliot <michael@digital.com.au>
2018-09-20 17:18:09 +0800

    DD-118: Style “View live/legacy addresses” with icon

--- src/components/WalletHWSelector.jsx
+++ src/components/WalletHWSelector.jsx
@@ -137,7 +137,7 @@ class WalletHardHWSelector extends React.Component {
                 this.props.network.hw.error
                 ?
                   <React.Fragment>
-                    <h2 className="connect-fail">{ capitalize(this.props.network.hw.option) } Connection Failed</h2>
+                    <h2 className="connect-fail">{ capitalize(this.props.network.hw.option.replace('-', ' ')) } Connection Failed</h2>
                     {
                       this.props.network.hw.option.substring(0, 6) === "ledger" &&
                       <div className="typo-c">
@@ -159,10 +159,10 @@ class WalletHardHWSelector extends React.Component {
                 :
                   this.props.network.hw.addresses.length > 0 &&
                   <React.Fragment>
-                    <h2 className="connect-success">{ capitalize(this.props.network.hw.option) } Connected</h2>
+                    <h2 className="connect-success">{ capitalize(this.props.network.hw.option.replace('-', ' ')) } Connected</h2>
                     <section style={ { width: "75%", margin: "0 auto" } }>
                       <p className="typo-c align-center" style={ {color: "#fff"} }><span className="green-dot"></span>{ settings.hwNetwork === "main" ? "Etherem" : "Test" } { settings.hwNetwork } Network</p>
-                      <div style={ {margin: "2.5rem 0 2.4rem"} }>
+                      <div style={ {margin: "2.5rem 0 0"} }>
                         <Select
                           name="wallet-address"
                           value={ value }
@@ -177,14 +177,23 @@ class WalletHardHWSelector extends React.Component {
                         {
                           this.props.network.hw.option.substring(0, 6) === "ledger" &&
                           <div>
-                            <a
-                              href="#action"
-                              onClick={ e => {
-                                                e.preventDefault();
-                                                this.loadLedgerAddresses(this.props.network.hw.option === "ledger-live" ? "legacy" : "live");
-                                              }}>
-                              View { this.props.network.hw.option === "ledger-live" ? "legacy" : "live" } addresses
-                            </a>
+                            <div style={ {margin: '0.5rem auto 2.4rem', textAlign: 'center'} }>
+                              <a
+                                className="switch-ledger-type"
+                                href="#action"
+                                onClick={ e => {
+                                                  e.preventDefault();
+                                                  this.loadLedgerAddresses(this.props.network.hw.option === "ledger-live" ? "legacy" : "live");
+                                                }}>
+                                <svg width="16" height="14" viewBox="0 0 16 14" xmlns="http://www.w3.org/2000/svg">
+                                  <g fill="none" fillRule="evenodd" stroke="#9aa3ad" strokeLinecap="round" strokeLinejoin="round" transform="translate(1 1)">
+                                    <path d="m14 1v4h-4"/><path d="m0 11.2v-4h4"/>
+                                    <path d="m1.59727273 3.999512c.65601558-1.94259944 2.21726092-3.39668309 4.13527336-3.85143401 1.91801245-.45475092 3.92455263.15342908 5.31472661 1.61088745l2.9527273 2.90737589m-14 2.66731734 2.95272727 2.90737593c1.39017401 1.4574583 3.39671419 2.0656383 5.31472664 1.6108874 1.91801239-.4547509 3.47925779-1.90883456 4.13527339-3.851434"/>
+                                  </g>
+                                </svg>
+                                View { this.props.network.hw.option === "ledger-live" ? "legacy" : "live" } addresses
+                              </a>
+                            </div>
                           </div>
                         }
                         <div className="align-center">
