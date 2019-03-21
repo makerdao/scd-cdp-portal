cb393811d572d79d7b4731ce2344a40feceea8a6
Tyler Sorensen <tyler@makerdao.com>
2019-03-21 13:15:39 -0700

    update trezor-connect to v7, add trezor manifest

--- src/utils/trezor-subprovider.js
+++ src/utils/trezor-subprovider.js
@@ -62,6 +62,11 @@ export default function createTrezorSubprovider(
 
   let alreadyOpenTrezorModal = false;
 
+  TrezorConnect.manifest({
+    email: 'tyler@makerdao.com',
+    appUrl: 'http://cdp.makerdao.com'
+  });
+
   async function getAccounts() {
     try {
       if (!alreadyOpenTrezorModal) {
