a158d77094340f3a6beb725c2d83394c8fb5f2f3
Ethan <ebennett19@gmail.com>
2019-05-05 20:36:46 -0600

    Improve mobile input fields

--- src/components/NewCupMobile.jsx
+++ src/components/NewCupMobile.jsx
@@ -34,7 +34,7 @@ export default class NewCupMobile extends Component {
             <label className="typo-cm no-select typo-bolder" style={{color: "#ffffff", marginTop: "8px"}}>How much ETH would you like to collateralize?</label>
             <div className="input-values-container">
               <input ref={ input => this.eth = input } type="number" id="inputETH" className="number-input" required step="0.000000000000000001" placeholder="0.000" value={ ethText } onChange={ e => { checkValues("eth", e.target.value) } } onKeyDown={ e => { if (e.keyCode === 38 || e.keyCode === 40 || e.keyCode === 189) e.preventDefault() } } />
-              <span className="unit" style={ {marginBottom: "0.35rem" } }>ETH</span>
+              <span className="unit">ETH</span>
               <div className="typo-cs align-right clearfix">
                 { printNumber(skr) } PETH
               </div>
@@ -51,7 +51,7 @@ export default class NewCupMobile extends Component {
             <label className="typo-cm no-select typo-bolder" style={{color: "#ffffff"}}>How much DAI would you like to generate?</label>
             <div className="input-values-container" style={{marginBottom: "25px"}}>
               <input ref={ input => this.dai = input } type="number" id="inputDAI" className="number-input" required step="0.000000000000000001" placeholder="0.000" value={ daiText } onChange={ e => { checkValues("dai", e.target.value) } } onKeyDown={ e => { if (e.keyCode === 38 || e.keyCode === 40 || e.keyCode === 189) e.preventDefault() } } />
-              <span className="unit" style={ {marginBottom: "0.35rem" } }>DAI</span>
+              <span className="unit">DAI</span>
               {
                 maxDaiAvail &&
                 <p className="typo-cs align-right">Max DAI available to generate: { printNumber(maxDaiAvail) } DAI</p>
